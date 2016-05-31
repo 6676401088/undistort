6e3c9425e9f42b860f56689246ea33662b3753c7
Alex Hagiopol <alexhagiopol@gmail.com>
2016-05-31 13:56:46 -0700

    Undistortion working.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 
-void undistort(const cv::Mat& input, cv::Mat& output, double f, double cx, double cy, const std::vector<double>& distortion) {
+using namespace std;
+
+void undistort(const cv::Mat& input,
+               cv::Mat& output,
+               double f,
+               double cx,
+               double cy,
+               double k1,
+               double k2,
+               double p1,
+               double p2) {
     //cout << "UNDISTORT: Starting..." << endl;
     int rows = input.rows;
     int cols = input.cols;
-    //double mapxArr[rows, cols];
-    //double mapyArr[rows, cols];
+
     cv::Mat mapx(rows,cols,CV_32FC1);
     cv::Mat mapy(rows,cols,CV_32FC1);
     for (int i = 0; i < rows; i++){
@@ -23,35 +32,24 @@ void undistort(const cv::Mat& input, cv::Mat& output, double f, double cx, doubl
             double theta4 = theta2 * theta2;
             double theta6 = theta4 * theta2;
             double theta8 = theta4 * theta4;
-            double theta_d = theta * (1 + distortion[0] * theta2 + distortion[1] * theta4 + distortion[2] * theta6 + distortion[3] * theta8);
+            double theta_d = theta * (1 + k1 * theta2 + k2 * theta4 + p1 * theta6 + p2 * theta8);
             double scale = (r == 0) ? 1.0 : theta_d / r;
             double u = f * x * scale + cx;
             double v = f * y * scale + cy;
 
             mapx.at<float>(i,j) = (float) u;
             mapy.at<float>(i,j) = (float) v;
-            //mapxArr[i, j, 0] = (float)u;
-            //mapyArr[i, j, 0] = (float)v;
         }
     }
-    //cout << "UNDISTORT: Maps made." << endl;
 
+    std::vector<cv::Mat> bgrChannels(3);
+    cv::split(input,bgrChannels);
     cv::Mat b_img(rows,cols,CV_8UC1);
     cv::Mat g_img(rows,cols,CV_8UC1);
     cv::Mat r_img(rows,cols,CV_8UC1);
-    const int input_to_b[] = {0,0};
-    const int input_to_g[] = {1,0};
-    const int input_to_r[] = {2,0};
-    size_t nPairs = 1;
-    size_t nSources = 3;
-    size_t nDestinations = 1;
-
-    //Line Take blue channel from input image and add it to single channel b_img
-    cv::mixChannels(&input, nSources, &b_img, nDestinations, input_to_b, nPairs);
-    cv::mixChannels(&input, nSources, &g_img, nDestinations, input_to_g, nPairs);
-    cv::mixChannels(&input, nSources, &r_img, nDestinations, input_to_r, nPairs);
-
-    //cout << "UNDISTORT: Single channel images made." << endl;
+    b_img = bgrChannels[0];
+    g_img = bgrChannels[1];
+    r_img = bgrChannels[2];
 
     cv::Mat bb_img(rows,cols,CV_8UC1);
     cv::Mat gg_img(rows,cols,CV_8UC1);
@@ -61,14 +59,6 @@ void undistort(const cv::Mat& input, cv::Mat& output, double f, double cx, doubl
     cv::remap(g_img,gg_img,mapx,mapy,0,0);
     cv::remap(r_img,rr_img,mapx,mapy,0,0);
 
-    //cout << "UNDISTORT: Maps applied to single channel images." << endl;
-
-    nSources = 1;
-    nDestinations = 3;
-    const int bb_to_output[] = {0,0};
-    const int gg_to_output[] = {0,1};
-    const int rr_to_output[] = {0,2};
-
     //merge undistorted grayscale images back into color image. Replaces mixchannels() with merge() because mixchannels() throws nasty error
     std::vector<cv::Mat> arrays;
     arrays.push_back(bb_img);
@@ -77,9 +67,18 @@ void undistort(const cv::Mat& input, cv::Mat& output, double f, double cx, doubl
     cv::merge(arrays,output);
 }
 
-int main(){
+int main() {
     std::cout << "Running Alex's undistort program. Parameters are hard-coded please forgive me." << std::endl;
-    //Fisheye drone camera parameters. Use a chessboard procedure e.g.
+    /* Camera parameters are defined according to the OpenCV conventions: http://docs.opencv.org/2.4/doc/tutorials/calib3d/camera_calibration/camera_calibration.html
+     * Parameters for a given camera can be computed using a chessboard optimization procedure e.g. http://wiki.ros.org/ethzasl_ptam/Tutorials/camera_calibration
+     * Parameters below are from one of Alex's previous projects.
+     */
+
+    //File name
+    std::string videoPath = "/home/alex/mono/data/Beck-roof.mp4";
+    int startFrameRange = 50; //which frame you want to examine
+    int endFrameRange = 60;
+
     //Calibration parameters
     double fx = 1223.80333938171;
     double fy = 1223.80333938171;
@@ -89,7 +88,55 @@ int main(){
     //Distortion parameters
     double k1 = -0.0568935551550414;
     double k2 = 0.00111003912518243;
-    double p1 =  0.000561203697652756;
+    double p1 = 0.000561203697652756;
     double p2 = -0.00201554554913136;
+
+    //Handle video input
+    cv::VideoCapture capture(videoPath);
+    if (!capture.isOpened()) {
+        cerr << "The video file could not be opened successfully!!!" << endl;
+        return 0;
+    }
+    int f = 0;
+    bool readSuccess = true;
+    while(readSuccess == true){
+        cv::Mat videoFrame;
+        readSuccess = capture.read(videoFrame); //extract video frame
+        if (readSuccess == true && f >= startFrameRange && f <= endFrameRange) {
+            cv::Mat undistortedFrame(videoFrame.rows, videoFrame.cols, CV_8UC3);
+            undistort(videoFrame,undistortedFrame,fx,cx,cy,k1,k2,p1,p2); //perform undistortion
+            cv::Mat concatenated;
+            cv::hconcat(videoFrame,undistortedFrame,concatenated);
+            cv::namedWindow("INPUT (LEFT) vs OUTPUT(RIGHT)",cv::WINDOW_NORMAL);
+            cv::resizeWindow("INPUT (LEFT) vs OUTPUT(RIGHT)",2560,1440);
+            cv::imshow("INPUT (LEFT) vs OUTPUT(RIGHT)",concatenated);
+            cv::waitKey(0);
+        }
+        f++;
+    }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
